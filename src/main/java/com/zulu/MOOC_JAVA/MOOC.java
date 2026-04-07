8ea2c41720264ec455b0fd0253b636251bbe12db
Zulu-Aggrippa <zulu.aggrippa72@gmail.com>
2026-04-07 20:21:29 +0200

    Learning Java

---
+++ src/main/java/com/zulu/MOOC_JAVA/MOOC.java
@@ -0,0 +1,18 @@
+package com.zulu.MOOC_JAVA;
+
+public class MOOC {
+     static void main() {
+       int item = 12;
+       String whoYouare = "Ras I";
+       double twicee = 3.28;
+       char theWeyou = 'R';
+       boolean isitMe = false;
+       if(!isitMe){
+           System.out.println("The you you are is");
+        System.out.println("You are "+whoYouare+". You've lived on Earth for "+item+" years.\nYour last GPA was "+twicee+".\nIn Severance you would be Helly "+theWeyou+"." );
+       }
+       else {
+           System.out.println("I don't know what to print now OG. 🤷🏾‍♂️");
+       }
+    }
+}
